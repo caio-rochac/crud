127117f6175b0613a857127a3c81f82444a696b4
caio-rochac <72214422+caio-rochac@users.noreply.github.com>
2020-10-14 17:18:59 -0300

    Update caiorocha_daniloaragao.py

--- caiorocha_daniloaragao.py
+++ caiorocha_daniloaragao.py
@@ -26,4 +26,30 @@ while opcao != -1:
                 print(alunos[i][1])
 				
 	if opcao == 2:
-		
+		alunos.append(["", "", 0, 0, 0, ""])
+		alunos[-1][0] = input("Digite o CPF: ")
+		alunos[-1][1] = input("Digite o nome: ")
+		alunos[-1][2] = int(input("Digite a primeira nota: "))
+		alunos[-1][3] = int(input("Digite a segunda nota: "))
+		alunos[-1][4] = (alunos[-1][2] + alunos[-1][3]) / 2
+		if alunos[-1][4] >= 7:
+			alunos[-1][5] = "A"
+		elif alunos[-1][4] < 7 and alunos[-1][4] >= 4:
+			alunos[-1][5] = "F"
+		else:
+			alunos[-1][5] = "R"
+	
+	if opcao == 3:
+		if len(alunos) > 0:
+			cpf = input("Consultar CPF: ")
+			contagem = len(alunos)
+
+			for i in range(alunos):
+				if cpf == alunos[i][0]:
+
+					contagem -= 1
+
+			if contagem == len(alunos):
+				print("CPF não encontrado!")
+		else:
+			print("Não há alunos cadastrados!")
